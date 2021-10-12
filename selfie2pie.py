de5219f78a7a7b9ff5bd9069c1ba2c3424046077
shenghaoc <shenghaoc@outlook.com>
2021-10-12 14:17:49 +0800

    Add files via upload

---
+++ selfie2pie.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from PIL import Image
+
+# PIE resolution
+width = 32
+height = 32
+
+# Follow PIE naming scheme
+cnt = 0
+
+# Target directory: resized
+Path('resized').mkdir(exist_ok=True)
+
+# Source directory: selfies
+# Accept any image format supported by Pillow
+# Assume all files are images!
+for filename in Path('selfies').glob('*'):
+    cnt = cnt + 1
+    im = Image.open(filename)
+    # Convert to grayscale and resize to PIE resolution
+    im_processed = im.convert('L').resize((width, height))
+    im_processed.save(Path('resized') / (str(cnt) + '.jpg'))
